3ee7d589b6de359145ca8cc480cc706e62080477
mstuff <mstuff@posteo.de>
2022-03-27 00:23:18 +0100

    Intermediate level finished

--- src/main/java/Intermediate.java
+++ src/main/java/Intermediate.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Intermediate {
@@ -8,18 +9,30 @@ public class Intermediate {
         // printNumberName(fetchNumberNameFromArray(userInput));
 
         //oneHundred();
-    }
 
+       /* ArrayList<String> textStorage = new ArrayList<>();
+        System.out.println("Please insert some text: ");
+        textStorage.add(getUserInputText());
+        printArrayList(textStorage);
+        */
+        scanSavePrint();
+
+
+
+
+
+    }
 
     public static String[] arrayOfNumbers = {"eins", "zwei", "drei", "vier", "fünf",
             "sechs", "sieben", "acht", "neun", "zehn"};
 
-    public static void printArrayContent() {
-        for (int i = 0; i < arrayOfNumbers.length; i++) {
-            System.out.println(arrayOfNumbers[i] + " ");
+    public static void printArrayContent(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
     }
-    public static int getUserInput(){
+
+    public static int getUserInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello! Please insert a number between 1 and 10.");
         return scan.nextInt();
@@ -29,8 +42,8 @@ public class Intermediate {
         return arrayOfNumbers[number - 1];
     }
 
-    public static void printNumberName(String numberName){
-        System.out.println("Your number is: " + numberName +  ".");
+    public static void printNumberName(String numberName) {
+        System.out.println("Your number is: " + numberName + ".");
     }
 
     public static void oneHundred() {
@@ -46,5 +59,34 @@ public class Intermediate {
         }
     }
 
-
+    public static String getUserInputText() {
+        Scanner scan = new Scanner(System.in);
+        //System.out.println("Please insert some text: ");
+        return scan.nextLine();
     }
+
+    public static void printArrayList (ArrayList<String> userInputText) {
+        for (int i = 0; i < userInputText.size(); i++) {
+        System.out.println(userInputText.get(i));
+        }
+    }
+
+    public static void scanSavePrint(){
+        String runAgain = "y";
+        while (runAgain == "y"){
+            //do something;
+            System.out.println("Continue? [y/n]");
+            runAgain = getUserInputText();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/Intermediate2.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Intermediate2 {
+    public static void main(String[] args) {
+
+        boolean runAgain = true;
+
+        String userPromptInput = "\nType in something: ";
+        String userPromptToContinue = "\nDo you want to type in more words? [y / n]";
+
+        ArrayList<String> arrayListForUserWords = makeStringArrayList();
+
+        while (runAgain){
+            System.out.println("\nThis is what you typed so far: \n");
+            printArrayList(arrayListForUserWords);
+            String userInput = returnStringFromUserPrompt(userPromptInput);
+            arrayListForUserWords.add(userInput);
+            runAgain = continueWhenUserEntersY(userPromptToContinue);
+        }
+    }
+
+    public static ArrayList<String> makeStringArrayList(){
+        return new ArrayList<>();
+
+    }
+
+    public static void printArrayList(ArrayList<String> userInputText) {
+        for (int i = 0; i < userInputText.size(); i++) {
+            System.out.println(userInputText.get(i));
+        }
+    }
+
+    public static String returnStringFromUserPrompt(String userPrompt){
+        System.out.println(userPrompt);
+        return readUserInput();
+    }
+
+    public static String readUserInput(){
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+    public static boolean continueWhenUserEntersY(String userPrompt){
+        String userInput = returnStringFromUserPrompt(userPrompt);
+        if (userInput.equals("y")){
+            return true;
+        } else {
+            System.out.println("program finished");
+            return false;
+        }
+    }
+}
